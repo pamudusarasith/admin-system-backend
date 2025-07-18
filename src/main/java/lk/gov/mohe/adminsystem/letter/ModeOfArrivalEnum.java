ca137ea6d94f2ccb4cc61eaa77c0e98e709d3294
Pamudu Sarasith <pamudu.lelkada@gmail.com>
2025-07-18 11:53:12 +0530

    feat: add Letter entity, repository, controller, and related enums for letter management

---
+++ src/main/java/lk/gov/mohe/adminsystem/letter/ModeOfArrivalEnum.java
@@ -0,0 +1,11 @@
+package lk.gov.mohe.adminsystem.letter;
+
+public enum ModeOfArrivalEnum {
+    REGISTERED_POST,
+    UNREGISTERED_POST,
+    EMAIL,
+    WHATSAPP,
+    HAND_DELIVERED,
+    FAX,
+    OTHER
+}
---
+++ src/main/java/lk/gov/mohe/adminsystem/letter/PriorityEnum.java
@@ -0,0 +1,7 @@
+package lk.gov.mohe.adminsystem.letter;
+
+public enum PriorityEnum {
+    NORMAL,
+    HIGH,
+    URGENT
+}

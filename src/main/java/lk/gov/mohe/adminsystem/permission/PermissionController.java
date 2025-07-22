53af9394e5943f53faa6a3124892e891984e3447
Sachinthaka Gayashan <sachinthakagayashan44@gmail.com>
2025-07-22 16:52:00 +0530

    Add PermissionController to handle permission retrieval

---
+++ src/main/java/lk/gov/mohe/adminsystem/permission/PermissionController.java
@@ -0,0 +1,19 @@
+package lk.gov.mohe.adminsystem.permission;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+
+public class PermissionController {
+
+    private final PermissionRepository permissionRepository;
+
+    @GetMapping("/permissions")
+    public ResponseEntity<?> getAllPermission() {
+        return ResponseEntity.ok(permissionRepository.findAll());
+    }
+}

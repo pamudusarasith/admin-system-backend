cc0fed3c6ec99303c21ab75393225fc09d4bd836
Pamudu Sarasith <pamudu.lelkada@gmail.com>
2025-10-14 01:21:56 +0530

    feat: refactor user retrieval and enhance event details with assigned user

--- src/main/java/lk/gov/mohe/adminsystem/user/UserController.java
+++ src/main/java/lk/gov/mohe/adminsystem/user/UserController.java
@@ -19,9 +19,13 @@ public class UserController {
   private final UserService userService;
 
   @GetMapping("/users")
-  @PreAuthorize("hasAuthority('user:read')")
-  public ApiResponse<List<UserDto>> getUsers() {
-    return ApiResponse.of(userService.getUsers());
+  @PreAuthorize("hasAnyAuthority('user:read', 'letter:assign:user')")
+  public ApiResponse<List<UserDto>> getUsers(
+      @RequestParam(defaultValue = "") String query,
+      @RequestParam(required = false) Integer divisionId,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int pageSize) {
+    return ApiResponse.paged(userService.getUsers(query, divisionId, page, pageSize));
   }
 
   @PostMapping("/users")
--- src/main/java/lk/gov/mohe/adminsystem/user/UserRepository.java
+++ src/main/java/lk/gov/mohe/adminsystem/user/UserRepository.java
@@ -1,12 +1,14 @@
 package lk.gov.mohe.adminsystem.user;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository
+    extends CrudRepository<User, Integer>, JpaSpecificationExecutor<User> {
   @NonNull
   List<User> findAll();
 

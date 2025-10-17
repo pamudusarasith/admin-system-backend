074b4bf3f70fe6259d83dd22750a9e058e36441e
Uthum Imasha <imashauthum@gmail.com>
2025-10-17 23:21:01 +0530

    Update AdminSystemApplication.java

--- src/main/java/lk/gov/mohe/adminsystem/AdminSystemApplication.java
+++ src/main/java/lk/gov/mohe/adminsystem/AdminSystemApplication.java
@@ -3,9 +3,11 @@ package lk.gov.mohe.adminsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class AdminSystemApplication {
 
   public static void main(String[] args) {
